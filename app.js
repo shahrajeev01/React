4b15ed99a3e8145899ffef1ea6cb4c787da04dc3
shahrajeev01 <rajeev01.shah@gmail.com>
2024-04-18 15:44:35 +0530

    add react using npm and used parcel

---
+++ app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+
+const heading = React.createElement("h1",{},"Rajeev - React JavaScript");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
